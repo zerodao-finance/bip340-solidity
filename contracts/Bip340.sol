6832d915a0584ae5d144e5e0390ead13ca801ac8
Trey Del Bonis <trey.delbonis@protonmail.com>
2022-12-28 03:53:00 -0500

    Wrapped computing the y coordinate of the pubkey in a separate function so we can leave it precomputed.

--- contracts/Bip340.sol
+++ contracts/Bip340.sol
@@ -9,8 +9,8 @@ contract Bip340 {
     /// Verifies a BIP340 signature parsed as `(rx, s)` form against a message
     /// `m` and a pubkey's x coord `px`.
     ///
-    /// p - public key x coordinate
-    /// r - signature r commitment
+    /// px - public key x coordinate
+    /// rx - signature r commitment
     /// s - signature s proof
     /// m - message hash
     function verify(uint256 px, uint256 rx, uint256 s, bytes32 m) public pure returns (bool) {
@@ -18,7 +18,17 @@ contract Bip340 {
         //
         // This *could* be precomputed and stored.
         uint256 py = EllipticCurve.deriveY(0x02, px, Secp256k1.AA, Secp256k1.BB, Secp256k1.PP);
+        return verifyFull(px, py, rx, s, m);
+    }
 
+    /// Verifies a BIP340 signature parsed as `(rx, s)` form against a message
+    /// `m` and pubkey coords `px` and `py`.
+    ///
+    /// px, py - public key coordinates
+    /// rx - signature r commitment
+    /// s - signature s proof
+    /// m - message hash
+    function verifyFull(uint256 px, uint256 py, uint256 rx, uint256 s, bytes32 m) public pure returns (bool) {
         // Let e = int(hashBIP0340/challenge(bytes(r) || bytes(P) || m)) mod n.
         uint256 e = computeChallenge(bytes32(rx), bytes32(px), m);
 
@@ -44,10 +54,10 @@ contract Bip340 {
     /// Done with jacobian coordinates to save gas.  Split out to avoid making
     /// the stack too deep.
     function computeRv(uint256 s, uint256 e, uint256 px, uint256 py) internal pure returns (uint256, uint256) {
-        // Let R = s⋅G - e⋅P.
+        // s⋅G - e⋅P.
         (uint256 sgx, uint256 sgy, uint256 sgz) = EllipticCurve.jacMul(s, Secp256k1.GX, Secp256k1.GY, 1, Secp256k1.AA, Secp256k1.PP);
         (uint256 epx, uint256 epy, uint256 epz) = EllipticCurve.jacMul(e, px, py, 1, Secp256k1.AA, Secp256k1.PP);
-        uint256 epy_inv = (Secp256k1.PP - epy) % Secp256k1.PP;
+        uint256 epy_inv = (Secp256k1.PP - epy) % Secp256k1.PP; // only have to flip the y coordinate
         (uint256 rvx, uint256 rvy, uint256 rvz) = EllipticCurve.jacAdd(sgx, sgy, sgz, epx, epy_inv, epz, Secp256k1.PP);
 
         // Convert back to affine now that we're done.  I don't think we
